79472961dc6f9fdc9ee2bb9b7d42957692364280
Rajesh Kalaria <rajesh.kalaria@evernym.com>
2018-06-21 14:00:25 +0530

    completed few tests and related code, added few more failing tests

--- library.py
+++ library.py
@@ -5,6 +5,9 @@ _mixed_ordinal_pat = _whole_word(r'-?\d+(st|th|nd|rd)')
 _integer_pat = _whole_word(r'\d+')
 _floating_point_after_pat = re.compile(r'\.\d+[^a-zA-Z.]')
 _floating_point_before_pat = re.compile(r'(?<=\d\.)')
+_date_iso8601_pat = _whole_word(r'\d{4}-(0\d|1[0-2])-(0[1-9]|[12][0-9]|3[01])')
+_date_other_pat = _whole_word(r'\d{2} (Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec) \d{4}')
+
 
 def mixed_ordinals(text):
     '''Find tokens that begin with a number, and then have an ending like 1st or 2nd.'''
@@ -19,6 +22,16 @@ def integers(text):
                 _floating_point_after_pat.match(text, match.end()):
             continue
         yield ('integer', match)
+
+def dates_iso8601(text):
+    '''Find tokens that are dates (iso8601)'''
+    for match in _date_iso8601_pat.finditer(text):
+        yield('date', match)
+
+def dates_other(text):
+    '''Find tokens that are dates in this format DD MMM YYYY (for example25 Jan 2017)'''
+    for match in _date_other_pat.finditer(text):
+        yield('date', match)
 
 def scan(text, *extractors):
     '''
--- library_test.py
+++ library_test.py
@@ -10,6 +10,7 @@ US until 15-20 years later.) It is celebrated by 77.9% of the population--
 trending toward 80.                                                                
 '''
 
+
 class TestCase(unittest.TestCase):
 
     # Helper function
@@ -29,6 +30,67 @@ class TestCase(unittest.TestCase):
     # Third unit test; prove that if we look for integers where there are none, we get no results.
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
+
+    # Fourth unit test; prove that if we can scan date from given text
+    def test_dates_ios8601(self):
+        self.assert_extract("I was born on 2015-07-25.", library.dates_iso8601, '2015-07-25')
+
+    # Fifth unit test; prove if invalid dates are not scanned
+    def test_no_dates(self):
+        self.assert_extract("I was born on 2015-13-25.", library.dates_iso8601)
+
+    # Sixth unit test; prove that we are able to scan other date formats
+    def test_dates_other_format(self):
+        self.assert_extract("I was born on 25 Jan 2017.", library.dates_other, '25 Jan 2017')
+
+    # ---------- test cases for new requirements
+
+    # Unit test; 2018-06-22 18:22:19.123".
+    def test_dates_ios8601_with_timestamp_with_space_delim(self):
+        self.assert_extract("I was born on 2018-06-22 18:22:19.123.", library.dates_iso8601,
+                            '2018-06-22 18:22:19.123')
+
+    # Unit test; 2018-06-22T18:22:19.123".
+    def test_dates_ios8601_with_timestamp_with_T_delim(self):
+        self.assert_extract("I was born on 2018-06-22T18:22:19.123.", library.dates_iso8601,
+                            '2018-06-22T18:22:19.123')
+
+    # Unit test; 2018-06-22 18:22:19.123".
+    def test_dates_ios8601_with_timestamp_till_millis(self):
+        self.assert_extract("I was born on 2018-06-22 18:22:19.123.", library.dates_iso8601, '2018-06-22 18:22:19.123')
+
+    # Unit test; 2018-06-22 18:22:19".
+    def test_dates_ios8601_with_timestamp_till_second(self):
+        self.assert_extract("I was born on 2018-06-22 18:22:19.", library.dates_iso8601, '2018-06-22 18:22:19')
+
+    # Unit test; 2018-06-22 18:22".
+    def test_dates_ios8601_with_timestamp_till_minute(self):
+        self.assert_extract("I was born on 2018-06-22 18:22.", library.dates_iso8601, '2018-06-22 18:22')
+
+    # Unit test; 2018-06-22 18:22:19.123 MDT".
+    def test_dates_ios8601_with_timestamp_with_timezone_format1(self):
+        self.assert_extract("I was born on 2018-06-22 18:22:19.123 MDT.", library.dates_iso8601, '2018-06-22 18:22:19.123 MDT')
+
+    # Unit test; 2018-06-22 18:22:19.123 +05:30".
+    def test_dates_ios8601_with_timestamp_with_timezone_format2(self):
+        self.assert_extract("I was born on 2018-06-22 18:22:19.123 +05:30.", library.dates_iso8601, '2018-06-22 18:22:19.123 +05:30')
+
+    # Unit test; 2018-06-22 18:22:19.123 +0530".
+    def test_dates_ios8601_with_timestamp_with_timezone_format3(self):
+        self.assert_extract("I was born on 2018-06-22 18:22:19.123 +0530.", library.dates_iso8601,
+                            '2018-06-22 18:22:19.123 +0530')
+
+    # Unit test; 2018-06-22 18:22:19.123 Z".
+    def test_dates_ios8601_with_timestamp_with_timezone_format4(self):
+        self.assert_extract("I was born on 2018-06-22 18:22:19.123 Z.", library.dates_iso8601, '2018-06-22 18:22:19.123 Z')
+
+    # Unit test; other date format with comma, 25 Jan, 2017
+    def test_dates_other_format_1(self):
+        self.assert_extract("I was born on 25 Jan, 2017.", library.dates_other, '25 Jan, 2017')
+
+    # Unit test; extracting numbers with comma in between
+    def test_integers_other_format_1(self):
+        self.assert_extract("123,456,789", library.integers, '123', '456', '789')
 
 
 if __name__ == '__main__':
